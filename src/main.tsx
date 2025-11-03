2ebad8a5102746ce6759a2ca84f38184aad7cfbc
nadia-dot-com <lezhanska2000@gmail.com>
2025-11-03 17:36:42 +0100

    Add .env to .gitignore and update project setup

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,16 @@ import './index.css'
 import App from './App.tsx'
 import { ShopProvider } from './context/ShopContext.tsx'
 import { UserProvider } from './context/UserContext.tsx'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <UserProvider>
-      <ShopProvider>
-        <App />
-      </ShopProvider>
-    </UserProvider>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+      <UserProvider>
+        <ShopProvider>
+          <App />
+        </ShopProvider>
+      </UserProvider>
+    </GoogleOAuthProvider>
   </StrictMode>,
 )
