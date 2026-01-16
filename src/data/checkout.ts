a11677c30660d1ac2990400cb735b629d98785f2
Nadiia Lezhanska <nadiia@Nadiias-MacBook-Pro.local>
2026-01-16 14:39:23 +0100

    Deleted data after integrat server

--- src/data/checkout.ts
+++ src/data/checkout.ts
@@ -1,9 +1,5 @@
 import { CheckoutItitial } from "../types/checkoutTypes";
 
-export const DEFAULT_VAT = 0;
-
-export const VAT_RATES = new Map<string, number>();
-
 export const CHECKOUT_INITIAL: CheckoutItitial = {
     items: [],
     shippingData: null,
