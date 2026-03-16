3f50725dc9fa56065a9f2369f46b81c38e1fb992
Nadiia Lezhanska <nadiia@Nadiias-MacBook-Pro.local>
2026-03-16 11:58:44 +0100

    fix ui plus

--- src/components/products/Products/ProductItem/ProductItem.tsx
+++ src/components/products/Products/ProductItem/ProductItem.tsx
@@ -76,7 +76,7 @@ const ProductItemVisual = memo(
           >
             <svg
               width="8"
-              height="10"
+              height="8"
               viewBox="0 0 14 14"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
