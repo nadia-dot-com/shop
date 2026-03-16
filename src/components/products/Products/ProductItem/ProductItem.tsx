b37944b719f0995063fca0ab10f0bc4d7065952f
Nadiia Lezhanska <nadiia@Nadiias-MacBook-Pro.local>
2026-03-16 11:55:24 +0100

    fix ui plus

--- src/components/products/Products/ProductItem/ProductItem.tsx
+++ src/components/products/Products/ProductItem/ProductItem.tsx
@@ -74,7 +74,20 @@ const ProductItemVisual = memo(
             onClick={addToCart}
             disabled={!isInStock}
           >
-            +
+            <svg
+              width="8"
+              height="10"
+              viewBox="0 0 14 14"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M7 1V13M1 7H13"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+              />
+            </svg>
           </button>
         </div>
       </div>
